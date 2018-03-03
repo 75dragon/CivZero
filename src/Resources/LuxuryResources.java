9939a24da7c350e3a3f3880d2f7a3794bbe82291
Austin Cheng <austinbicheng@gmail.com>
2018-03-03 11:50:11 -0800

    rescaling imgs

--- src/Resources/LuxuryResources.java
+++ src/Resources/LuxuryResources.java
@@ -28,8 +28,9 @@ public enum LuxuryResources implements YieldModifier, Drawable
 		y = new Yields(foodMod, prodMod, goldMod);
 		this.name= name;
 		loadImg(pic);
+		img = scale(img,100,100);
 	}
-
+	
 	public void loadImg(String pic)
 	{
 		try
@@ -40,6 +41,26 @@ public enum LuxuryResources implements YieldModifier, Drawable
 		{
 			e.printStackTrace();
 		}
+	}
+	
+	public static BufferedImage scale(BufferedImage src, int w, int h)
+	{
+	    BufferedImage img = 
+	            new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+	    int x, y;
+	    int ww = src.getWidth();
+	    int hh = src.getHeight();
+	    int[] ys = new int[h];
+	    for (y = 0; y < h; y++)
+	        ys[y] = y * hh / h;
+	    for (x = 0; x < w; x++) {
+	        int newX = x * ww / w;
+	        for (y = 0; y < h; y++) {
+	            int col = src.getRGB(newX, ys[y]);
+	            img.setRGB(x, y, col);
+	        }
+	    }
+	    return img;
 	}
 	
 	@Override
--- src/Resources/Resources.java
+++ src/Resources/Resources.java
@@ -12,7 +12,7 @@ import Tiles.Yields;
 
 public enum Resources implements YieldModifier, Drawable
 {
-	WHEAT(1, 0, "Wheat", "Wheat"), CATTLE(1, 0, "Cattle", "Cattle"), HORSE(0, 1, "Horse", "Horses"), SHEEP(0, 1, "Sheep", "Sheep");
+	WHEAT(1, 0, "Wheat", "Wheat"), CATTLE(1, 0, "Cattle", "Cattle"), HORSE(0, 1, "Horse", "Horse"), SHEEP(0, 1, "Sheep", "Sheep");
 	Yields y;
 	String name;
 	BufferedImage img;
@@ -21,6 +21,7 @@ public enum Resources implements YieldModifier, Drawable
 		this.name = name;
 		y = new Yields(foodMod, prodMod);
 		loadImg(pic);
+		img = scale(img,100,100);
 	}
 	
 	public void loadImg(String pic)
@@ -33,6 +34,26 @@ public enum Resources implements YieldModifier, Drawable
 		{
 			e.printStackTrace();
 		}
+	}
+	
+	public static BufferedImage scale(BufferedImage src, int w, int h)
+	{
+	    BufferedImage img = 
+	            new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+	    int x, y;
+	    int ww = src.getWidth();
+	    int hh = src.getHeight();
+	    int[] ys = new int[h];
+	    for (y = 0; y < h; y++)
+	        ys[y] = y * hh / h;
+	    for (x = 0; x < w; x++) {
+	        int newX = x * ww / w;
+	        for (y = 0; y < h; y++) {
+	            int col = src.getRGB(newX, ys[y]);
+	            img.setRGB(x, y, col);
+	        }
+	    }
+	    return img;
 	}
 
 	@Override
