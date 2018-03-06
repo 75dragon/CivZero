9b915927854dd0e1c5c98710596aca3232b9b98b
Austin Cheng <austinbicheng@gmail.com>
2018-03-06 08:13:26 -0800

    dunno honestly

--- src/Units/Scout.java
+++ src/Units/Scout.java
@@ -4,6 +4,6 @@ public class Scout extends Unit
 {
 	public Scout(int xCor, int yCor)
 	{
-		super(xCor, yCor, 2, "Scout");
+		super(xCor, yCor, 2, "Scout", img);
 	}
 }
--- src/Units/Unit.java
+++ src/Units/Unit.java
@@ -29,8 +29,49 @@ public class Unit
 		name = s;
 	}
 
-	public void moveUnit()
+	public static BufferedImage scale(BufferedImage src, int w, int h)
 	{
+	    BufferedImage img = 
+	            new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+	    int x, y;
+	    int ww = src.getWidth();
+	    int hh = src.getHeight();
+	    int[] ys = new int[h];
+	    for (y = 0; y < h; y++)
+	        ys[y] = y * hh / h;
+	    for (x = 0; x < w; x++) {
+	        int newX = x * ww / w;
+	        for (y = 0; y < h; y++) {
+	            int col = src.getRGB(newX, ys[y]);
+	            img.setRGB(x, y, col);
+	        }
+	    }
+	    return img;
+	}
+	
+	public void moveUnit(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
 
+	public int getSight()
+	{
+		return sight;
+	}
+
+	public void setSight(int sight)
+	{
+		this.sight = sight;
+	}
+
+	public int getX()
+	{
+		return x;
+	}
+
+	public int getY()
+	{
+		return y;
 	}
 }
